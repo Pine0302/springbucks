4b26bdf40eb7efda542e908e4c6bc4a18b73d769
沈朝松 <shenchaosong@yidaoit.com>
2019-12-30 19:59:35 +0800

    spring_generate_1

--- src/main/java/com/example/springbucks/SpringbucksApplication.java
+++ src/main/java/com/example/springbucks/SpringbucksApplication.java
@@ -5,12 +5,18 @@ import com.example.springbucks.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @Slf4j
@@ -25,7 +31,8 @@ public class SpringbucksApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception{
-		Coffee c = Coffee.builder().name("espresso")
+		generateArtifacts();
+		/*Coffee c = Coffee.builder().name("espresso")
 				.price(Money.of(CurrencyUnit.of("CNY"),20.0))
 				.build();
 		int count = coffeeMapper.save(c);
@@ -37,7 +44,17 @@ public class SpringbucksApplication implements ApplicationRunner {
 		count = coffeeMapper.save(c);
 		log.info("Save {} Coffee: {}",count ,c);
 		c = coffeeMapper.findById(c.getId());
-		log.info("Find Coffee: {}",c);
+		log.info("Find Coffee: {}",c);*/
+
+	}
+
+	private function generateArtifacts() throws Exception{
+		List<String> warnings = new ArrayList<>();
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(this.getClass().getResourceAsStream("/generaatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(true);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,callback,warnings);
+		myBatisGenerator.generate();
 
 	}
 
