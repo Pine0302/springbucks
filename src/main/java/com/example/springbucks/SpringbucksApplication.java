178ca04c705369b23fa12129cf9c764d5b13324f
shenchaosong <shenchaosong@yidaoit.cn>
2020-08-09 13:49:48 +0800

    draw into mongodb

--- src/main/java/com/example/springbucks/SpringbucksApplication.java
+++ src/main/java/com/example/springbucks/SpringbucksApplication.java
@@ -1,10 +1,13 @@
 package com.example.springbucks;
 
+import com.example.springbucks.converter.MoneyReadConverter;
 import com.example.springbucks.mapper.CoffeeMapper;
 import com.example.springbucks.mapper.CoffeeMapperPageHelper;
 import com.example.springbucks.model.Coffee;
 import com.example.springbucks.model.CoffeeExample;
+import com.example.springbucks.model.MongoCoffee;
 import com.github.pagehelper.PageInfo;
+import com.mongodb.Mongo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.RowBounds;
 import org.joda.money.CurrencyUnit;
@@ -19,8 +22,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -28,6 +37,10 @@ import java.util.List;
 @Slf4j
 @MapperScan("com.example.springbucks.mapper")
 public class SpringbucksApplication implements ApplicationRunner {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
 	@Autowired
 	private CoffeeMapper coffeeMapper;
 
@@ -42,7 +55,8 @@ public class SpringbucksApplication implements ApplicationRunner {
 	public void run(ApplicationArguments args) throws Exception{
 		//generateArtifacts();
 		//playWithArtifacts();
-		palyWithPageHelper();
+		//palyWithPageHelper();
+		playWithMongoCoffee();
 	}
 
 	private void generateArtifacts() throws Exception{
@@ -99,6 +113,28 @@ public class SpringbucksApplication implements ApplicationRunner {
 		list4.forEach(e->log.info("coffee_list4: {}",e));
 		PageInfo page = new PageInfo(list4);
 		log.info("pageinfo: {}",page);
+	}
+
+	@Bean
+	public MongoCustomConversions mongoCustomerConversions(){
+		return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
+	}
+
+
+	public void playWithMongoCoffee(){
+		MongoCoffee espresso = MongoCoffee.builder()
+				.name("espresso")
+				.price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+				.createTime(new Date())
+				.updateTime(new Date())
+				.build();
+		MongoCoffee saved = mongoTemplate.save(espresso);
+		log.info("saved coffee {} ", saved);
+
+		List<MongoCoffee> list = mongoTemplate.find(
+				Query.query(Criteria.where("name").is("espresso")), MongoCoffee.class);
+		log.info("find {} coffee.", list.size());
+		list.forEach(c -> log.info("coffee:{}", c));
 	}
 
 
